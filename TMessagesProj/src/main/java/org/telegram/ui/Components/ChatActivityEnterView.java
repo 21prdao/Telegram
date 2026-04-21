f55c4b8ca9e177b32a3d90fc8f1080b51d5e28f5
21prdao <21prdao@gmail.com>
2026-04-21 18:24:07 +0800

    fix(wallet): avoid BC ECDSA provider dependency when creating wallets

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
@@ -217,6 +217,7 @@ import me.vkryl.android.animator.BoolAnimator;
 import me.vkryl.android.animator.FactorAnimator;
 
 import org.telegram.wallet.config.WalletConfig;
+import org.telegram.wallet.data.WalletStorage;
 import org.telegram.wallet.navigation.WalletNavigator;
 
 public class ChatActivityEnterView extends FrameLayout implements
@@ -3689,6 +3690,15 @@ public class ChatActivityEnterView extends FrameLayout implements
                 return;
             }
             if (parentFragment == null) {
+                return;
+            }
+            if (!WalletStorage.hasAnyWallet(getContext())) {
+                org.telegram.ui.ActionBar.AlertDialog.Builder builder = new org.telegram.ui.ActionBar.AlertDialog.Builder(getContext());
+                builder.setTitle(getString(R.string.WalletRequiredTitle));
+                builder.setMessage(getString(R.string.WalletRequiredMessage));
+                builder.setPositiveButton(getString(R.string.WalletCreateNow), (dialog, which) -> WalletNavigator.openWalletManager(getContext()));
+                builder.setNegativeButton("取消", null);
+                parentFragment.showDialog(builder.create());
                 return;
             }
             WalletNavigator.openCreateRedPacket(parentFragment, currentAccount, dialog_id);
